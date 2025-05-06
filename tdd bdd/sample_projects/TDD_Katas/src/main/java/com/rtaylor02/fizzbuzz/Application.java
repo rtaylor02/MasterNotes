332714284ef329ff63181a60a576430251856d37
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2025-05-06 14:23:51 +0100

    Add implementation and test cases for FIZZ BUZZ app

--- tdd bdd/sample_projects/TDD_Katas/src/main/java/com/rtaylor02/fizzbuzz/Application.java
+++ tdd bdd/sample_projects/TDD_Katas/src/main/java/com/rtaylor02/fizzbuzz/Application.java
@@ -1,4 +1,25 @@
 package com.rtaylor02.fizzbuzz;
 
 public class Application {
+    public String compute(int number) {
+        StringBuilder result = new StringBuilder();
+
+        if (divisibleBy(number, 3)) {
+            result.append("FIZZ");
+        }
+
+        if (divisibleBy(number, 5)) {
+            result.append("BUZZ");
+        }
+
+        if (!(divisibleBy(number, 3) || divisibleBy(number, 5))) {
+            result.append(Integer.toString(number));
+        }
+
+        return result.toString();
+    }
+
+    private static boolean divisibleBy(int number, int factor) {
+        return number % factor == 0;
+    }
 }
--- tdd bdd/sample_projects/TDD_Katas/src/test/java/com/rtaylor02/fizzbuzz/ApplicationTest.java
+++ tdd bdd/sample_projects/TDD_Katas/src/test/java/com/rtaylor02/fizzbuzz/ApplicationTest.java
@@ -1,7 +1,28 @@
 package com.rtaylor02.fizzbuzz;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ApplicationTest {
+    private Application app = new Application();
 
+    @ParameterizedTest(name = "{0}: {1}")
+    @CsvSource({"1, 1", "2, 2", "3, FIZZ", "4, 4", "5, BUZZ", "6, FIZZ", "7, 7", "15, FIZZBUZZ"})
+    void testCsvData(int number, String expected) {
+        // ARRANGE - ACT - ASSERT
+        assertEquals(expected, app.compute(number));
+    }
+
+    @ParameterizedTest(name = "{0}: {1}")
+    @CsvFileSource(resources = "")
+    void testMediumSizeCsvFile() {
+
+    }
 }
